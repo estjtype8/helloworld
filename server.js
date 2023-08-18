9c4c121607160cede23ef27db8279e760dccfc3a
gizem tekiner kuvandık <92186218+estjtype8@users.noreply.github.com>
2023-08-18 13:31:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Gizem');
+  res.send('Hello Guys!!');
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
